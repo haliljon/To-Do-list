84ed67ddbc60602d5f36400312fed99f7dd3fc57
haliljon <110017001+haliljon@users.noreply.github.com>
2022-12-16 01:12:24 +0500

    Add and remove functions

--- src/index.js
+++ src/index.js
@@ -1,34 +1,116 @@
 import reload from './icons8-refresh-30.svg';
 import './style.css';
 
+const information = JSON.parse(localStorage.getItem('lists')) || [];
 const liList = document.createElement('li');
 liList.className = 'list-item';
 const ulList = document.querySelector('.to-do-list');
 const clearBtn = document.querySelector('.clear');
+const input = document.querySelector('#input');
+const inputBtn = document.querySelector('.input-btn');
 
-const reloadImg = document.getElementById('reload');
-reloadImg.src = reload;
+class ToDoList {
+  editBtn = document.querySelectorAll('.edit-btn');
+  displayList() {
+    for (let i = 0; i < information.length; i += 1) {
+      const liList = document.createElement('li');
+      liList.className = 'list-item';
+      liList.innerHTML = `<input type="checkbox" class="square"/><p>${information[i].description}</p><button type="button"
+      name="edit" class="edit-btn"><ion-icon class="list-icon" name="ellipsis-vertical"> </ion-icon></button><button type="button" name="delete" class="delete-btn"><ion-icon class="list-icon" name="trash-outline"></ion-icon></button>`;
+      ulList.insertBefore(liList, clearBtn);
+    }
+  }
 
-const toDoArr = [
-  {
-    square: '<ion-icon class="square" name="square-outline"></ion-icon>',
-    description: 'was the dishes',
-    icon: "<ion-icon class='list-icon' name='ellipsis-vertical'></ion-icon>",
-    completed: false,
-    index: 1,
-  },
-  {
-    square: '<ion-icon class="square" name="square-outline"></ion-icon>',
-    description: 'complete To Do list project',
-    icon: "<ion-icon class='list-icon' name='ellipsis-vertical'></ion-icon>",
-    completed: false,
-    index: 2,
-  },
-];
-
-for (let i = 0; i < toDoArr.length; i += 1) {
-  const liList = document.createElement('li');
-  liList.className = 'list-item';
-  liList.innerHTML = toDoArr[i].square + toDoArr[i].description + toDoArr[i].icon;
-  ulList.insertBefore(liList, clearBtn);
+  addList() {
+    const newList = { description: input.value, completed: false };
+    information.push(newList);
+    localStorage.setItem('lists', JSON.stringify(information));
+    const liList = document.createElement('li');
+    liList.className = 'list-item';
+    liList.innerHTML = `<input type="checkbox" class="square"/><p>${
+      information[information.length - 1].description
+    }</p><button type="button"
+    name="edit" class="edit-btn"><ion-icon class="list-icon" name="ellipsis-vertical"> </ion-icon></button><button type="button" name="delete" class="delete-btn"><ion-icon class="list-icon" name="trash-outline"></ion-icon></button>`;
+    ulList.insertBefore(liList, clearBtn);
+  }
+  editList(edit) {
+    const chosenItem = edit.previousSibling;
+    chosenItem.setAttribute('contentEditable', true);
+    chosenItem.focus();
+    return chosenItem;
+  }
 }
+
+const listCollection = new ToDoList();
+listCollection.displayList();
+
+inputBtn.addEventListener('click', (e) => {
+  if (input.value !== '') {
+    listCollection.addList();
+    input.value = '';
+  } else {
+    e.preventDefault();
+    alert('Please fill ot the field');
+  }
+  const editBtn = editBtnFun();
+  const deleteBtn = document.querySelectorAll('.delete-btn');
+  editBtn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      listCollection.editList(btn);
+
+      editBtn[index].style.display = 'none';
+      deleteBtn[index].style.display = 'block';
+      const chosenItem = btn.previousSibling;
+      const data = JSON.parse(localStorage.getItem('lists'));
+      const listItem = document.querySelectorAll('.list-item');
+      listItem[index].addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+          chosenItem.setAttribute('contentEditable', false);
+          const newText = chosenItem.textContent;
+          data[index] = { description: newText, completed: false };
+          localStorage.setItem('lists', JSON.stringify(data));
+        }
+      });
+    });
+  });
+  deleteBtn.forEach((element, index) => {
+    element.addEventListener('click', () => {
+      ulList.removeChild(element.parentElement);
+      information.splice(index, 1);
+      localStorage.setItem('lists', JSON.stringify(information));
+    });
+  });
+});
+function editBtnFun() {
+  return document.querySelectorAll('.edit-btn');
+}
+const editBtn = editBtnFun();
+
+const deleteBtn = document.querySelectorAll('.delete-btn');
+editBtn.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    listCollection.editList(btn);
+
+    editBtn[index].style.display = 'none';
+    deleteBtn[index].style.display = 'block';
+    const chosenItem = btn.previousSibling;
+    const data = JSON.parse(localStorage.getItem('lists'));
+    const listItem = document.querySelectorAll('.list-item');
+    listItem[index].addEventListener('keypress', function (e) {
+      if (e.key === 'Enter') {
+        chosenItem.setAttribute('contentEditable', false);
+        const newText = chosenItem.textContent;
+        data[index] = { description: newText, completed: false };
+        localStorage.setItem('lists', JSON.stringify(data));
+      }
+    });
+  });
+});
+
+deleteBtn.forEach((element, index) => {
+  element.addEventListener('click', () => {
+    ulList.removeChild(element.parentElement);
+    information.splice(index, 1);
+    localStorage.setItem('lists', JSON.stringify(information));
+  });
+});
